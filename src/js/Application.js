515a48a963149d26bcdbb5e53787a63f7b3f9b40
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-01-24 11:41:07 +0200

    add bananas

--- src/js/Application.js
+++ src/js/Application.js
@@ -11,11 +11,18 @@ export default class Application extends EventEmitter {
     super();
     this.emojis = [];
     this.banana = "🍌";
+    this.mixedArr=[];
+    this.emojisContainer = document.getElementById("emojis");
     this.emit(Application.events.READY);
   }
   setEmojis(emojis) {
-    this.emojis = emojis;
+   this.emojis = emojis;
   }
 
-  addBananas() {}
+  addBananas() {
+    this.emojis = this.emojis.map((emoji) => emoji + this.banana);
+    const p = document.createElement("p");
+    p.textContent = this.emojis.reduce((prev, curr) => prev + curr);
+    this.emojisContainer.appendChild(p);
+  }
 }
